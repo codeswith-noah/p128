ccf903ba87d03858b723fdbf5644f4f52aa2c484
codeswith-noah <85592454+codeswith-noah@users.noreply.github.com>
2022-06-04 12:09:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+    song1 = loadSound("imperial march.mp3");
+    song2 = loadSound("gfalls.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    //I added a canvas ID so I could style it to not come up on top of the heading and button
+
+    canvas.id('myCanvas');
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded(){
+    console.log('PoseNet has been successfully initialized.');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = " + leftWristX + " Left wrist y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + " Right Wrist Y = " + rightWristY);
+    }
+}
